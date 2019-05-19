108482231ffdac74f824464b587891dad4e7e4c4
FabNaneix <40290670+FabNaneix@users.noreply.github.com>
2019-05-19 19:35:11 -0400

    Added Micro_anal function

--- QPP analysis.py
+++ QPP analysis.py
@@ -4,15 +4,20 @@ Data extraction and analyses for Quinine Protein Preference QPP
 
 Created by Fabien Naneix
 """
-
 import numpy as np
 import GF_Naneix as GF
 
 ## Metadata folder (check pathway for each exp)
-#metafile="C:\\Users\\fabie\\Documents\\QPP data\\QPPh1_metafile.txt"
-metafile="C:\\Users\\fabie\\Documents\\QPP data\\QPPh1_metafile_pref.txt"
+#metafile="C:\\Users\\fabie\\Documents\\QPP data\\QPPh1_metafile.txt" #metafile with all data
+metafile="C:\\Users\\fabie\\Documents\\QPP data\\QPPh1_metafile_pref.txt" #metafile with only preference tests
 medfolder="C:\\Users\\fabie\\Documents\\QPP data\\Behaviour\\"
 
+
+"""
+Extract function (from JEM_general_function)
+Goes through the metafile (as .txt)
+Extract each column of interest and return a list for each
+"""
 ## Extraction of data and creation of lists for each column from the metafile
 def Extract (metafile):
     f = open(metafile, 'r')
@@ -42,7 +47,15 @@ def Extract (metafile):
              'Diet':Diet, 'Date':Date, 'Left':Left, 'Right':Right})
     #create a dict for each column
     
-    
+"""
+ListMaker function
+Use the following arguments
+date: date of the session of interest
+LLicks: list of left licks with the same index than 'date'
+RLicks: list of Right licks with the same index than 'date'
+Return for each session ('date') 4 lists of n animals as following:
+NR_casein / NR_malto / PR_casein / NR_malto
+"""    
 def ListsMaker(date, LLicks, RLicks):
     PrefT_C_LicksNR = []
     PrefT_M_LicksNR = []
@@ -93,7 +106,7 @@ for session in Licksallrats:
     Trials.append(session[2])
 
 
-#Create different list for lefyt and right licks for forced choices (trials 1 to 45) and free choices (45 to 65)    
+#Create different list for left and right licks for forced choices (trials 1 to 45) and free choices (45 to 65)    
 LLicks_forced = []
 LLicks_free = []
 RLicks_forced = []
@@ -179,6 +192,26 @@ Quin1_0_t1_free = ListsMaker('20/11/2018', LLicks_free, RLicks_free)
 Quin1_0_t2_free = ListsMaker('21/11/2018', LLicks_free, RLicks_free)
 
 
+"""
+Micro_anal function:
+- Go throught a list of list (Quin_cond) corresponding of lick data for/
+1 specific quinine day and condition (total, forced or free)
+- Run lickCalc on each list to analyse licking microstructure
+- return a list of dictionaries of 23 parameters
+- each list is 1 rat/1 bottle in the following order:
+NR_casein (4) / NR_Malto (4) / PR_casein (4) / PR_Malto (see ListsMaker function)
+   
+"""
+def Micro_anal(Quin_cond):
+    Lick_patterns_dicts = []
+    for l in Quin_cond:
+        for lst in l:
+            x = GF.lickCalc(lst)
+            Lick_patterns_dicts.append(x)
+    return Lick_patterns_dicts
+    
+
+Micro_Quin0_t1 = Micro_anal(Quin0_t1)
 
 
 
